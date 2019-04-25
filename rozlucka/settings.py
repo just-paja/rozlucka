9fabc5b2e51ea2bcdd801322081fca2850b65da2
Pavel Žák <pavel@berrycloud.cz>
2019-04-25 13:43:45 +0200

    Include local settings

--- rozlucka/settings.py
+++ rozlucka/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '../static')
+
+
+try:
+    # pylint: disable=wildcard-import
+    from local_settings import * # noqa
+except ImportError:
+    pass
